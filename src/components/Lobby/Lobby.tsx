4d6175148284814ae97c9d4b958972bdcc577d1f
Manuel Pickl <picklmanuel98@gmail.com>
2024-02-16 01:50:06 +0100

    build fix

--- src/components/Lobby/Lobby.tsx
+++ src/components/Lobby/Lobby.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppContext } from "../AppContext";
 import { Player } from "../../types/class/Player";
 import { useConnectionManagerContext } from "../ConnectionManager/ConnectionManagerContext";
